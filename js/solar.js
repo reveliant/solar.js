2d1f11405a912d668f24aac008c3bd13662ecb0a
Rémi Dubois <packman@oxiame.net>
2015-10-13 23:03:26 +0200

    Changing the Canvas class and adding private methods

--- js/solar.js
+++ js/solar.js
@@ -1,127 +1,161 @@
 /*jshint nocomma: true, nonew: true, plusplus: true, strict: true, browser: true, devel: true, jquery: true*/
 
-var SolarCanvas = false;
-
 function Canvas(id) {
     'use strict';
+    var my = this,  // Public scope
+        priv = {};  // Private scope
+
+    // HTML environment
     this.element = document.getElementById(id);
     this.container = this.element.parentElement;
     this.context = this.element.getContext('2d');
+
+    // Properties initialization
     this.center = { x: 0, y: 0 };
     this.zoomFactor = 1;
     this.scaleFactor = 1;
+
+    // Default style
+    this.context.strokeStyle = '#333333';
+    this.context.fillStyle = '#666666';
+    this.context.font = '9px sans-serif';
+
+
+    this.scale = function (scale) {
+        my.scaleFactor = scale;
+    };
+
+    this.recenter = function () {
+        my.center = {
+            x: my.context.canvas.width / 2,
+            y: my.context.canvas.height / 2
+        };
+    };
+
+    this.resize = function () {
+        console.log('Resizing canvas #' + my.element.getAttribute('id'));
+        my.context.canvas.width = my.container.clientWidth;
+        //this.context.canvas.height = this.container.clientHeight;
+        my.recenter();
+    };
     this.resize();
+
+    // Geometrical private methods
+    priv.point = function (point) {
+        // Ensure parameter is a cartesian point
+        point = point || {};
+        point.x = point.hasOwnProperty('x') ? point.x : null;
+        point.y = point.hasOwnProperty('y') ? point.y : null;
+        return point;
+    };
+    priv.cartesian = function (coords) {
+        // Canvas affine frames are (O,x→,y↓), we've to mirror
+        // on X-axis to get a usual (O,x→,y↑) affine frame
+        coords = priv.point(coords);
+        return {
+            x: my.center.x + coords.x * my.zoomFactor / my.scaleFactor,
+            y: my.center.y - coords.y * my.zoomFactor / my.scaleFactor // minus to mirror on X-axis
+        };
+    };
+
+    // Draw methods namespace
+    this.draw = {
+        line: function (from, to) {
+            from = priv.point(from);
+            to = priv.point(to);
+            my.context.beginPath();
+            my.context.moveTo(from.x, from.y);
+            my.context.lineTo(to.x, to.y);
+            my.context.stroke();
+        },
+        circle: function (center, radius) {
+            center = priv.point(center);
+            radius = radius || 0;
+            my.context.beginPath();
+            my.context.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+            my.context.closePath();
+            my.context.stroke();
+        },
+        ellipse: function (center, apapsis, periapsis, semiMajor, eccentricity, angleOffset, color) {
+            var semiMinor = Math.sqrt(apapsis * periapsis),
+                orbitX = semiMajor * eccentricity * Math.cos(angleOffset) + semiMajor,
+                orbitY = semiMajor * eccentricity * Math.sin(angleOffset),
+                angle, nodes = 1000;
+            center = priv.point(center);
+
+            my.context.beginPath();
+            my.context.moveTo(center.x + orbitX / my.scaleFactor, center.y + orbitY / my.scaleFactor);
+            for (angle = 0; angle <= nodes; angle = angle + 1) {
+                orbitX = semiMajor * eccentricity * Math.cos(angleOffset) + semiMajor * Math.cos( (2 * Math.PI) * (angle / nodes));
+                orbitY = semiMajor * eccentricity * Math.sin(angleOffset) + semiMinor * Math.sin( (2 * Math.PI) * (angle / nodes));
+                my.context.lineTo(center.x + orbitX / my.scaleFactor, center.y + orbitY / my.scaleFactor);
+            }
+            my.context.closePath();
+            my.context.stroke();
+        }
+    };
+
+    // Frame private methods and public drawer
+    priv.frame = {
+        zeroCross: function () {
+            var topCenter = { x: my.center.x, y: 0 },
+                bottomCenter = { x: my.center.x, y: my.context.canvas.height },
+                middleLeft = { x: 0, y: my.center.y },
+                middleRight = { x: my.context.canvas.width, y: my.center.y };
+            my.draw.line(topCenter, bottomCenter);
+            my.draw.line(middleLeft, middleRight);
+        },
+        xCross: function () {
+
+        },
+        rangeRing: function (radius, text) {
+            my.draw.circle(my.center, radius / my.scaleFactor);
+            if (text) {
+                if (radius >= 1000) text = radius / 1000 + " K";
+                if (radius >= 1000000) text = radius / 1000000 + " M";
+                my.context.fillText(text, my.center.x - radius / my.scaleFactor + 3, my.center.y + 10);
+            }
+        }
+    };
+    this.draw.frame = function (options) {
+        var i;
+
+        options = options || {};
+        options.zero = options.hasOwnProperty('zero') ? options.zero : true;
+        options.zeroCross = options.hasOwnProperty('zeroCross') ? options.zeroCross : true;
+        options.xCross = options.hasOwnProperty('xCross') ? options.xCross : true;
+        options.rings = options.hasOwnProperty('rings') ? options.rings : true;
+
+        console.debug('Frame drawing options', options);
+
+        if (options.zero) {
+            priv.frame.rangeRing(3 * my.scaleFactor, false);
+        }
+        if (options.zeroCross) {
+            priv.frame.zeroCross();
+        }
+        if (options.xCross) {
+            priv.frame.xCross();
+        }
+        if (options.rings) {
+            for (i = 1; i <= 50; i=i+1) {
+                priv.frame.rangeRing(i * 50 * my.scaleFactor, true);
+            }
+        }
+    };
+
+    this.render = function () {
+        my.draw.frame();
+    };
 }
 
-Canvas.prototype.scale = function (scale) {
-    'use strict';
-    this.scaleFactor = scale;
-};
-
-Canvas.prototype.resize = function () {
-    'use strict';
-    console.log("Resizing canvas #" + this.element.getAttribute("id"));
-    this.context.canvas.width = this.container.clientWidth;
-    //this.context.canvas.height = this.container.clientHeight;
-    this.center = {
-        x: this.context.canvas.width / 2,
-        y: this.context.canvas.height / 2
-    };
-};
-
-Canvas.prototype.cartesian = function (coords) {
-    'use strict';
-    // Canvas affine frames are (O,x→,y↓), we've to mirror
-    // on X-axis to get a usual (O,x→,y↑) affine frame
-    return {
-        x: this.center.x + coords.x * this.zoomFactor / this.scaleFactor,
-        y: this.center.y - coords.y * this.zoomFactor / this.scaleFactor // minus to mirror on X-axis
-    };
-
-};
-
-Canvas.prototype.line = function (from, to) {
-    'use strict';
-    this.context.beginPath();
-    this.context.moveTo(from.x, from.y);
-    this.context.lineTo(to.x, to.y);
-    this.context.stroke();
-};
-
-Canvas.prototype.circle = function (center, radius) {
-    'use strict';
-    this.context.beginPath();
-    this.context.arc(center.x, center.y, radius / this.scaleFactor, 0, Math.PI * 2);
-    this.context.closePath();
-    this.context.stroke();
-};
-
-
-Canvas.prototype.zeroCross = function () {
-    'use strict';
-    var topCenter = { x: this.center.x, y: 0 },
-        bottomCenter = { x: this.center.x, y: this.context.canvas.height },
-        middleLeft = { x: 0, y: this.center.y },
-        middleRight = { x: this.context.canvas.width, y: this.center.y };
-    this.line(topCenter, bottomCenter);
-    this.line(middleLeft, middleRight);
-};
-
-Canvas.prototype.rangeRing = function (radius, text) {
-    'use strict';
-    this.circle(this.center, radius);
-    if (text && radius >= 1000) text = radius / 1000 + " K";
-    if (text && radius >= 1000000) text = radius / 1000000 + " M";
-    if (text) this.context.fillText(text, this.center.x - radius / this.scaleFactor + 3, this.center.y + 10);
-};
-
-Canvas.prototype.drawFrame = function (options) {
-    'use strict';
-    var i;
-
-    options = options || {};
-    options.zero = options.hasOwnProperty('zero') ? options.zero : true;
-    options.zeroCross = options.hasOwnProperty('zeroCross') ? options.zeroCross : true;
-    options.xCross = options.hasOwnProperty('xCross') ? options.xCross : true;
-    options.rings = options.hasOwnProperty('rings') ? options.rings : true;
-
-    console.debug("Frame drawing options", options);
-
-    /*this.context.strokeStyle = "#282828";
-    this.context.fillStyle = "#666";
-    this.context.font = "9px sans-serif";*/
-
-    if (options.zero) {
-        this.circle(this.center, 3 * this.scaleFactor);
-    }
-    if (options.zeroCross) {
-        this.zeroCross();
-    }
-    if (options.xCross) {
-        //this.xCross();
-    }
-    if (options.rings) {
-        for (i = 1; i <= 50; i=i+1) {
-            if (i !== 0) this.rangeRing(i * 50 * this.scaleFactor, true);
-        }
-    }
-};
-
-Canvas.prototype.drawEllipse = function (center, halfaxis, excentricity) {
-    'use strict';
-
-};
-
-Canvas.prototype.draw = function () {
-    'use strict';
-    this.drawFrame();
-};
+var SolarCanvas = null;
 
 $(document).ready(function() {
     'use strict';
     SolarCanvas = new Canvas('view');
     SolarCanvas.scale(500000);
-    SolarCanvas.draw();
+    SolarCanvas.render();
 
     $('canvas').scroll(function(){
         console.log('Scroll by');
@@ -131,6 +165,6 @@ $(document).ready(function() {
 $(window).resize(function() {
     'use strict';
     SolarCanvas.resize();
-    SolarCanvas.draw();
+    SolarCanvas.render();
     console.log('Window resized');
 });
